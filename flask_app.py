886eff5e7cfa7d546742c622153d2a115cfbb58f
Ekram21 <ekram_lol@hotmail.com>
2018-08-10 05:46:18 +0600

    a

---
+++ flask_app.py
@@ -0,0 +1,18 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/Login')
+def Login():
+    return render_template('Login.html')
+
+@app.route('/Profile')
+def Profile():
+    return render_template('Profile.html')
